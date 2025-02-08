215bd540efcb15b45a2457db8681635437e52c2d
krickovicx <krickovic.milica@gmail.com>
2025-02-09 00:57:43 +0100

    place real or fake obstacle

--- object.gd
+++ object.gd
@@ -6,8 +6,3 @@ func _ready() -> void:
 	anchor_left = randf() * 0.75
 	anchor_top = randf() * 0.75
 	texture = tile.sprite
-
-
-func _on_gui_input(event: InputEvent) -> void:
-	if event.is_action_pressed():
-		
