a53ebf526818292ed5123675e919c54edd78eb33
Mark Mozarovski <mark.mozh@gmail.com>
2025-02-08 19:05:16 +0100

    add tile placement

--- main.gd
+++ main.gd
@@ -1,13 +1,30 @@
+@tool
+
 extends Node3D
 
-const TILE = preload("res://tile.tscn")
+const TILE = preload("res://tiles/base_tile/tile.tscn")
+const STAIRS = preload("res://tiles/stairs/stairs.tscn")
 
-@export var map_size : Vector2
+@export var map_size : Vector3i = Vector3i(4, 4, 4)
 @export var tile_size : float = 5.0
+@export var tile_height : float = 2.5
+ 
+func tilemap_to_global(pos: Vector2i, level : int = 0):
+	return Vector3(pos.x, 0, pos.y) * tile_size + Vector3.UP * level * tile_height
 
 func _ready() -> void:
+	generate_grid()
+	place_item(STAIRS, Vector2i(0, 1),  0, 1)
+	
+func generate_grid():
 	for i in range(map_size.x):
-		for j in range(map_size.x):
+		for j in range(map_size.y):
 			var tile =  TILE.instantiate()
-			tile.global_position = Vector3(i, 0, j) * tile_size
 			add_child(tile)
+			tile.global_position = tilemap_to_global(Vector2i(i, j))
+
+func place_item(item : PackedScene, pos : Vector2i, level : int, rot: int = 0):
+	var inst = item.instantiate()
+	add_child(inst)
+	inst.global_position = tilemap_to_global(pos, level)
+	inst.global_rotation.y = rot * PI / 2
