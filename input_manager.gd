3d1e894baaa6a649012b7f8e8709869f5d105628
krickovicx <krickovic.milica@gmail.com>
2025-02-09 00:28:49 +0100

    add placing objects

--- input_manager.gd
+++ input_manager.gd
@@ -7,6 +7,10 @@ var controler_sens: float = 0.1
 func tile_selected(event: InputEvent) -> bool:
 	return (Global.player1 and event.is_action_pressed(p1_controls.jump)) \
 	or (!Global.player1 and event.is_action_pressed(p2_controls.jump))
+	
+func rotation(event:InputEvent) -> bool:
+	return (Global.player1 and event.is_action_pressed(p1_controls.rot)) \
+	or (!Global.player1 and event.is_action_pressed(p2_controls.rot))
 
 func get_player2_input():
 	var move_dir = Vector2.ZERO
@@ -31,27 +35,3 @@ func get_player1_input():
 	elif Input.is_action_just_pressed(p1_controls.right):
 		move_dir = Vector2(1, 0)
 	return move_dir
-	
-func get_player1_rot_input():
-	var rot = 0
-	if Input.is_action_just_pressed(p1_controls.fwd):
-		rot = 3
-	elif Input.is_action_just_pressed(p1_controls.back):
-		rot = 1
-	elif Input.is_action_just_pressed(p1_controls.left):
-		rot = 0
-	elif Input.is_action_just_pressed(p1_controls.right):
-		rot = 2
-	return rot
-	
-func get_player2_rot_input():
-	var rot = 0
-	if Input.is_action_just_pressed(p2_controls.fwd):
-		rot = 3
-	elif Input.is_action_just_pressed(p2_controls.back):
-		rot = 1
-	elif Input.is_action_just_pressed(p2_controls.left):
-		rot = 0
-	elif Input.is_action_just_pressed(p2_controls.right):
-		rot = 2
-	return rot
