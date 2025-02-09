37851ca69cc22628500faf639f98c3f8a851d426
krickovicx <krickovic.milica@gmail.com>
2025-02-09 10:34:59 +0100

    fix obstacles

--- tiles/ground_tile/ground_tile.gd
+++ tiles/ground_tile/ground_tile.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	var p = randf()
 	if p < 0.3:
 		has_obstacle = true
-		var ind = randi_range(0, 6)
+		var ind = randi_range(0, 5)
 		p = randf()
 		get_children()[ind].global_rotation.y += p*100
 		get_children()[ind].visible = true
